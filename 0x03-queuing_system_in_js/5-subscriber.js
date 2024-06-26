a20093d899ab2cd8a5d32a770b690bbf1a5b72d6
millyanne93 <nmillyanne20@gmail.com>
2024-06-26 14:19:15 +0000

    fix error msg

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -2,27 +2,20 @@ import { createClient } from 'redis';
 
 const client = createClient();
 
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
 client.on('connect', () => {
   console.log('Redis client connected to the server');
-  
-  client.subscribe('holberton school channel', (err) => {
-    if (err) {
-      console.error('Error subscribing to channel:', err.message);
-    } else {
-      console.log('Subscribed to holberton school channel');
-    }
-  });
 });
 
-client.on('error', (err) => {
-  console.error('Redis client not connected to the server:', err.message);
-});
+client.subscribe('holberton school channel');
 
-client.on('message', (channel, message) => {
-  console.log(message);
-  
-  if (message === 'KILL_SERVER') {
-    client.unsubscribe('holberton school channel');
+client.on('message', (_err, msg) => {
+  console.log(msg);
+  if (msg === 'KILL_SERVER') {
+    client.unsubscribe();
     client.quit();
   }
 });
